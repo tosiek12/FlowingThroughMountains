8e8686a0562712da9e9f23a345723cc5fb136aa1
tosiek12 <antek.trad@gmail.com>
2016-02-08 10:51:10 +0100

    initial

---
+++ FlowingThroughMountains/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlowingThroughMountains.Startup))]
+namespace FlowingThroughMountains
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
